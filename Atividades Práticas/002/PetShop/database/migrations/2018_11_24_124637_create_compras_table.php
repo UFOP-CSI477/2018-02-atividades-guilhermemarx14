21aacb43bd67440387d761d611eb9046f2ee1adf
Guilherme Marx <guilhermemarx14@gmail.com>
2018-11-24 11:02:03 -0200

    Migrations

---
+++ Atividades Práticas/002/PetShop/database/migrations/2018_11_24_124637_create_compras_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComprasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('compras', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('produto_id');
+            $table->smallInteger('quantidade')->default('1');
+            $table->dateTime('data');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('produto_id')->references('id')->on('produtos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('compras');
+    }
+}
